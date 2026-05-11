fd66d8143409aa4e1ecc5d1548fd6e0b53089d42
GihLuciano <giovanalucianoribeiro08@gmail.com>
2026-05-11 09:03:31 -0300

    Exibir dados de vendas para usuário

--- ctrl_home.py
+++ ctrl_home.py
@@ -23,5 +23,24 @@ def dashboard(): # função que gerencia rota
     """ Painel de Vendas"""
     # if 'user' not in session:
     #     return redirect(url_for("auth.login"))
+
+    import locale
+    # Define para o padrão brasileiro
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+    vendas: list = [
+        {"mes":"Janeiro", "total": 139519.19},
+        {"mes":"Fevereiro", "total": 139594.99},
+        {"mes":"Março", "total": 128519.31},
+        {"mes":"Abril", "total": 114529.99},
+        {"mes":"Maio", "total": 157893.22},
+        {"mes":"Junho", "total": 139429.29},
+        {"mes":"Julho", "total": 145893.17},
+        {"mes":"Agosto", "total": 199519.19},
+        {"mes":"Setembro", "total": 109549.60},
+        {"mes":"Outubro", "total": 119519.19},
+        {"mes":"Novembro", "total": 179519.19},
+        {"mes":"Dezembro", "total": 209529.29},
+    ] # fim lista vendas
     
-    return render_template("dashboard/index.html", title="Painel de Vendas")  # Renderiza um template
+    return render_template("dashboard/index.html", vendas=vendas, locale=locale) # Renderiza um template
